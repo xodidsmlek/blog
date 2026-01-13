fbee3a41bcd97e5c8d1cdd79b2ef31404a9f3682
TaeYang <xodidsmlek@gmail.com>
2026-01-13 15:31:31 +0900

    config파일 수정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-//   base: '/blog/', // ⭐ GitHub Pages 배포용
   base: command === 'build' ? '/blog/' : '/',
-})
+}))
